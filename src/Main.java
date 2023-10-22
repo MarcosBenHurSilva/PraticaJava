6a11301c081750db825b922014f7aebc42480d57
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-21 23:16:17 -0300

    Print e Println

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello mom!");
+		System.out.print("Hello world!");
 
 	}
 
